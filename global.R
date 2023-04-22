06be82c3211079539f1cebfb39a023f8e4594d91
AntonyAlexia <130075316+AntonyAlexia@users.noreply.github.com>
2023-04-22 17:25:31 -0400

    Update global.R

--- global.R
+++ global.R
@@ -1,3 +1,4 @@
-library(shinythemes)
-library(plotly)
-library(shinyjs)
+
+if (!require('shiny')) install.packages("shiny") 
+if (!require('shinythemes')) install.packages("shinythemes") 
+if (!require('shinyjs')) install.packages("shinyjs") 
